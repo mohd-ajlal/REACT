8f62b90618c3a4a6180dccd61320cae6a8e26c00
Mohd Ajlal <ajlal00786@gmail.com>
2023-11-23 03:13:52 +0530

    react

---
+++ first-vite-project/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    // <h1>Hello React with Vite| Mohd Ajlal</h1>
+    <div>
+      <h1>Hello React with Vite| Mohd Ajlal</h1>
+      <p>My first paragraph.</p>
+    </div>
+  )
+}
+
+export default App
