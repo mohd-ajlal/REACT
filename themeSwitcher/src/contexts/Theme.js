29c2a0d0cc7e8f6e91ae94d8de25d179187f01f8
Mohd Ajlal <ajlal00786@gmail.com>
2023-12-30 23:09:37 +0530

    REACT

--- themeSwitcher/src/contexts/Theme.js
+++ themeSwitcher/src/contexts/Theme.js
@@ -2,19 +2,12 @@ import { createContext, useContext } from "react";
 
 export const ThemeContext = createContext({
     themeMode: "light",
-    darkTheme: () => {
+    darkTheme: () => {},
+    lightTheme: () => {},
+})
 
-    },
-    lightTheme: () => {
-        
-    },
-
-});
-
-export const useProvider = ThemeContext.Provider
+export const ThemeProvider = ThemeContext.Provider
 
 export default function useTheme(){
     return useContext(ThemeContext)
 }
-
-
